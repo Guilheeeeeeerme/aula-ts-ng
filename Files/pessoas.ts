402adb588017155a0a174b8e662faaf15e3302a8
Your Name <you@example.com>
2019-09-10 19:03:46 -0300

    initial first class

---
+++ Files/pessoas.ts
@@ -0,0 +1,319 @@
+const Pessoas = [
+    {
+      "_id": "5d781d40efb4c5e1d41f00cc",
+      "index": 0,
+      "guid": "874c3641-edae-441f-a611-318bbaa708e2",
+      "isActive": false,
+      "balance": "$1,188.43",
+      "picture": "http://placehold.it/32x32",
+      "age": 39,
+      "eyeColor": "blue",
+      "name": "Kaufman Berg",
+      "gender": "male",
+      "company": "ENTALITY",
+      "email": "kaufmanberg@entality.com",
+      "phone": "+1 (815) 451-3950",
+      "address": "852 Kaufman Place, Elliston, Georgia, 3279",
+      "about": "Est ea aliquip quis mollit aliquip pariatur labore officia eu ex. Culpa veniam fugiat elit dolore magna pariatur ut consequat nostrud sit fugiat anim mollit fugiat. Nisi nostrud voluptate labore veniam eu ea irure velit. Irure Lorem laboris veniam commodo non magna cupidatat ipsum minim enim ut aliqua laborum. Pariatur fugiat amet enim velit culpa ullamco sint elit. Proident minim cillum irure adipisicing Lorem ad excepteur pariatur quis ea do.\r\n",
+      "registered": "2019-08-13T10:05:45 +03:00",
+      "latitude": -68.614173,
+      "longitude": -137.638738,
+      "tags": [
+        "quis",
+        "veniam",
+        "nostrud",
+        "pariatur",
+        "sit",
+        "nostrud",
+        "voluptate"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Simmons Curry"
+        },
+        {
+          "id": 1,
+          "name": "Bowman Doyle"
+        },
+        {
+          "id": 2,
+          "name": "Mattie Goff"
+        }
+      ],
+      "greeting": "Hello, Kaufman Berg! You have 10 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5d781d40bedd8e10e5059e9c",
+      "index": 1,
+      "guid": "ab6e3670-2645-4ee5-9093-4a0a19aa28e6",
+      "isActive": true,
+      "balance": "$2,773.32",
+      "picture": "http://placehold.it/32x32",
+      "age": 38,
+      "eyeColor": "blue",
+      "name": "Rosie Saunders",
+      "gender": "female",
+      "company": "AUSTECH",
+      "email": "rosiesaunders@austech.com",
+      "phone": "+1 (838) 465-3857",
+      "address": "522 Holt Court, Nipinnawasee, Federated States Of Micronesia, 8780",
+      "about": "Elit voluptate velit cillum Lorem magna. Culpa laborum id minim sunt laborum elit pariatur. Non velit adipisicing ipsum amet sit est elit anim.\r\n",
+      "registered": "2018-09-15T08:43:20 +03:00",
+      "latitude": -50.923972,
+      "longitude": 79.269162,
+      "tags": [
+        "et",
+        "nulla",
+        "aliquip",
+        "cillum",
+        "officia",
+        "ex",
+        "consequat"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Powers Wilkerson"
+        },
+        {
+          "id": 1,
+          "name": "Margret Warner"
+        },
+        {
+          "id": 2,
+          "name": "Rowland Nelson"
+        }
+      ],
+      "greeting": "Hello, Rosie Saunders! You have 9 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5d781d4034f9fe5d4831a8e5",
+      "index": 2,
+      "guid": "65e4f549-301a-43dd-bc2e-b2545eeff404",
+      "isActive": false,
+      "balance": "$3,293.01",
+      "picture": "http://placehold.it/32x32",
+      "age": 37,
+      "eyeColor": "brown",
+      "name": "Herminia Lawrence",
+      "gender": "female",
+      "company": "ZOGAK",
+      "email": "herminialawrence@zogak.com",
+      "phone": "+1 (816) 451-2815",
+      "address": "422 Hinckley Place, Ruckersville, Nevada, 6218",
+      "about": "Non in do culpa irure do laborum dolor occaecat dolore voluptate elit enim nulla commodo. Aliquip nostrud ex dolore elit id do labore incididunt est ex Lorem. Dolore non nulla magna veniam deserunt. Pariatur esse amet commodo mollit sint et sint sunt. Consequat eiusmod minim sit incididunt consequat laboris ut pariatur. Et sunt laboris minim ipsum voluptate.\r\n",
+      "registered": "2015-09-24T03:57:18 +03:00",
+      "latitude": 23.941292,
+      "longitude": -120.29391,
+      "tags": [
+        "aliquip",
+        "sunt",
+        "occaecat",
+        "dolor",
+        "incididunt",
+        "ea",
+        "laborum"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Sheena Kennedy"
+        },
+        {
+          "id": 1,
+          "name": "Chandra Henderson"
+        },
+        {
+          "id": 2,
+          "name": "Knight Jimenez"
+        }
+      ],
+      "greeting": "Hello, Herminia Lawrence! You have 2 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "5d781d4013907e58bb56dd01",
+      "index": 3,
+      "guid": "7195ac99-e5fa-4ee9-880a-a8d930351e37",
+      "isActive": false,
+      "balance": "$1,751.03",
+      "picture": "http://placehold.it/32x32",
+      "age": 23,
+      "eyeColor": "green",
+      "name": "Evangeline Nash",
+      "gender": "female",
+      "company": "GRACKER",
+      "email": "evangelinenash@gracker.com",
+      "phone": "+1 (825) 455-3884",
+      "address": "537 Kenilworth Place, Hannasville, Nebraska, 5516",
+      "about": "Sunt ipsum minim cillum mollit deserunt labore labore cillum magna non eiusmod magna ipsum minim. Minim aliquip do nisi consequat. Ut nostrud quis occaecat ad est amet. Eu sit magna est ipsum pariatur anim fugiat labore Lorem. Excepteur reprehenderit id amet et adipisicing nulla velit mollit.\r\n",
+      "registered": "2018-07-29T11:50:50 +03:00",
+      "latitude": -26.135118,
+      "longitude": 88.620021,
+      "tags": [
+        "duis",
+        "incididunt",
+        "culpa",
+        "veniam",
+        "labore",
+        "commodo",
+        "nostrud"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Faulkner Page"
+        },
+        {
+          "id": 1,
+          "name": "Simpson Welch"
+        },
+        {
+          "id": 2,
+          "name": "Mcintyre Anderson"
+        }
+      ],
+      "greeting": "Hello, Evangeline Nash! You have 4 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5d781d400cb116b1be75e2d7",
+      "index": 4,
+      "guid": "a33ca967-3146-4251-b6fd-80d383a880c6",
+      "isActive": false,
+      "balance": "$1,312.48",
+      "picture": "http://placehold.it/32x32",
+      "age": 40,
+      "eyeColor": "brown",
+      "name": "Wagner Gamble",
+      "gender": "male",
+      "company": "CIPROMOX",
+      "email": "wagnergamble@cipromox.com",
+      "phone": "+1 (908) 596-3968",
+      "address": "771 Irving Place, Outlook, South Dakota, 5747",
+      "about": "Esse nulla est aliqua sit minim mollit Lorem non et voluptate occaecat minim. Tempor ullamco duis exercitation quis et veniam reprehenderit qui et excepteur esse veniam. Velit duis ad excepteur minim ea. Et sunt ad veniam enim non consequat culpa nulla pariatur aute magna ex.\r\n",
+      "registered": "2016-03-29T04:53:59 +03:00",
+      "latitude": -55.549317,
+      "longitude": -163.00727,
+      "tags": [
+        "ut",
+        "proident",
+        "laboris",
+        "et",
+        "eu",
+        "magna",
+        "mollit"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Allie Briggs"
+        },
+        {
+          "id": 1,
+          "name": "Singleton Clayton"
+        },
+        {
+          "id": 2,
+          "name": "Whitney Duke"
+        }
+      ],
+      "greeting": "Hello, Wagner Gamble! You have 2 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "5d781d40b398cf51c6811e19",
+      "index": 5,
+      "guid": "0628a1b1-007f-49af-ad6b-fc961970d8a2",
+      "isActive": false,
+      "balance": "$1,897.69",
+      "picture": "http://placehold.it/32x32",
+      "age": 21,
+      "eyeColor": "green",
+      "name": "Bowen Whitney",
+      "gender": "male",
+      "company": "MITROC",
+      "email": "bowenwhitney@mitroc.com",
+      "phone": "+1 (862) 514-2810",
+      "address": "763 Tech Place, Allendale, Virginia, 7775",
+      "about": "Do dolore sunt ex aute sint. Aliquip sit ut quis quis ex anim ad nulla est adipisicing minim cillum. Minim Lorem nostrud proident elit voluptate amet ipsum laborum ipsum ad eu nulla dolor. Est ex deserunt anim velit nulla. Officia sit consequat et laboris.\r\n",
+      "registered": "2016-08-07T09:44:42 +03:00",
+      "latitude": 88.888066,
+      "longitude": 156.161574,
+      "tags": [
+        "enim",
+        "ad",
+        "aliquip",
+        "sunt",
+        "tempor",
+        "nisi",
+        "proident"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Blair Vazquez"
+        },
+        {
+          "id": 1,
+          "name": "Horne Sherman"
+        },
+        {
+          "id": 2,
+          "name": "Buchanan Carlson"
+        }
+      ],
+      "greeting": "Hello, Bowen Whitney! You have 5 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5d781d40f4e8266c73f2a3a1",
+      "index": 6,
+      "guid": "cecf7c70-70e8-4ee3-aed7-c7a3f6246320",
+      "isActive": true,
+      "balance": "$1,481.12",
+      "picture": "http://placehold.it/32x32",
+      "age": 37,
+      "eyeColor": "blue",
+      "name": "Ayala Hebert",
+      "gender": "male",
+      "company": "DIGIRANG",
+      "email": "ayalahebert@digirang.com",
+      "phone": "+1 (900) 428-3396",
+      "address": "929 Windsor Place, Clarksburg, Idaho, 9329",
+      "about": "Reprehenderit sunt veniam amet magna aute elit consectetur enim duis. Nulla pariatur et ad eiusmod quis aliquip velit est incididunt id. Labore minim laboris occaecat labore laboris ea ex cupidatat ex irure. Aliqua velit tempor exercitation minim ut sit.\r\n",
+      "registered": "2018-12-05T12:32:31 +02:00",
+      "latitude": -16.574842,
+      "longitude": 112.585333,
+      "tags": [
+        "id",
+        "reprehenderit",
+        "adipisicing",
+        "elit",
+        "excepteur",
+        "id",
+        "minim"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Shelia Conrad"
+        },
+        {
+          "id": 1,
+          "name": "Araceli Kemp"
+        },
+        {
+          "id": 2,
+          "name": "Maddox Miles"
+        }
+      ],
+      "greeting": "Hello, Ayala Hebert! You have 10 unread messages.",
+      "favoriteFruit": "strawberry"
+    }
+  ];
+
+  export { Pessoas };
