781b69433173d8d833fd89d3dc5bf9c95bfef19c
Your Name <you@example.com>
2019-09-18 20:37:10 -0300

    saving work

--- Angular/hello-world/src/app/app.module.ts
+++ Angular/hello-world/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { DetalhePessoaComponent } from './components/detalhe-pessoa/detalhe-pessoa.component';
+import { ListaPessoaComponent } from './components/lista-pessoa/lista-pessoa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetalhePessoaComponent,
+    ListaPessoaComponent
   ],
   imports: [
     BrowserModule,
