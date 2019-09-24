56ec248f0a67f3caf1c1b1e0d4fb0189b3d3b0c0
Your Name <you@example.com>
2019-09-24 17:03:45 -0300

    saving work

--- GuitarShop/guitar-shop-app/src/app/url.ts
+++ GuitarShop/guitar-shop-app/src/app/url.ts
@@ -1,5 +1,5 @@
 //  const host = '127.0.0.1';
-const host = 'http://ec2-18-222-134-19.us-east-2.compute.amazonaws.com';
+const host = 'ec2-18-222-134-19.us-east-2.compute.amazonaws.com';
 const port = 8081;
 
 const ApiUrl = `http://${host}:${port}/api`;
