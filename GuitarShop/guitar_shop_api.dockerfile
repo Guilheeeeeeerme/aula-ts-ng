61199630473de12fb6cd396b5d5267168913eebd
Your Name <you@example.com>
2019-09-23 22:10:30 -0300

    saving work

--- GuitarShop/guitar_shop_api.dockerfile
+++ GuitarShop/guitar_shop_api.dockerfile
@@ -1,3 +1,5 @@
-FROM php:7.2-apache
+FROM php:5.6-apache
+
+RUN docker-php-ext-install pdo pdo_mysql
 
 COPY ./ /var/www/html/
--- GuitarShop/guitar_shop_api/view/header.php
+++ GuitarShop/guitar_shop_api/view/header.php
@@ -5,6 +5,9 @@
     <title>My Guitar Shop</title>
     <link rel="stylesheet" type="text/css"
           href="/book_apps/ch05_guitar_shop/main.css">
+          <?php 
+              date_default_timezone_set('America/Sao_Paulo');
+          ?>
 </head>
 
 <!-- the body section -->
