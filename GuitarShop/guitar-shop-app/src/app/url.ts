e559e56108fdbfe5c9702b3563e53775de1df94b
Guilherme <ferreiraga@outlook.com>
2019-09-24 11:40:16 -0300

    Update url.ts

--- GuitarShop/guitar-shop-app/src/app/url.ts
+++ GuitarShop/guitar-shop-app/src/app/url.ts
@@ -1,4 +1,7 @@
-const host = '127.0.0.1';
-const ApiUrl = `http://${host}:8080/api`;
+//  const host = '127.0.0.1';
+const host = 'http://ec2-18-222-134-19.us-east-2.compute.amazonaws.com';
+const port = 8081;
+
+const ApiUrl = `http://${host}:${port}/api`;
 
 export { ApiUrl };
