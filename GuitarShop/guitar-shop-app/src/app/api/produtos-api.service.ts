350ca813e2e29ec7cc871050b6f892688a79f1ef
Guilherme <ferreiraga@outlook.com>
2019-09-25 14:03:25 -0300

    Update produtos-api.service.ts

--- GuitarShop/guitar-shop-app/src/app/api/produtos-api.service.ts
+++ GuitarShop/guitar-shop-app/src/app/api/produtos-api.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-const host = '127.0.0.1';
-// const host = 'ec2-18-222-134-19.us-east-2.compute.amazonaws.com';
+// const host = '127.0.0.1';
+const host = 'ec2-18-222-134-19.us-east-2.compute.amazonaws.com';
 const port = 8081;
 const ApiUrl = `http://${host}:${port}/api`;
 
